cd50f5a82403b99de0d0f720f9d9ff1085846fea
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2025-12-14 13:57:47 +0100

    Add alx_book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE Customers(
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE);
 
 CREATE TABLE Order_Details(
