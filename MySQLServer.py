491c84bed268ce3cf12aafdf6bd38cd511f351d4
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2025-12-14 15:38:19 +0100

    Add MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "ALX",
+    password = "123456"
+)
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except Error as e:
+    print(f"Error: {e}")
+
+
+
+mycursor.close()
+mydb.close()
+
+
